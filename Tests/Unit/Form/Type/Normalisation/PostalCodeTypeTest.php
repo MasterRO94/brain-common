9edc1d8d07c9fc8bf161f6a1f5f083ed1333bae4
Matthew Rose <matthew.rose@printed.com>
2018-06-22 12:34:41 +0100

    BRN-736 ~ Post code form type and a few Brazilian post codes from Marlon.

--- Tests/Unit/Form/Type/Normalisation/PostalCodeTypeTest.php
+++ Tests/Unit/Form/Type/Normalisation/PostalCodeTypeTest.php
@@ -2,7 +2,7 @@
 
 namespace Brain\Common\Tests\Unit\Form\Type\Normalisation;
 
-use Brain\Common\Form\Type\Normalisation\PhoneNumberType;
+use Brain\Common\Form\Type\Normalisation\PostalCodeType;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormInterface;
@@ -11,7 +11,7 @@ use Symfony\Component\Form\Test\TypeTestCase;
 /**
  * {@inheritdoc}
  */
-final class PhoneNumberTypeTest extends TypeTestCase
+final class PostalCodeTypeTest extends TypeTestCase
 {
     /**
      * @test
@@ -20,23 +20,23 @@ final class PhoneNumberTypeTest extends TypeTestCase
      * @group form
      * @group normalisation
      *
-     * @covers \Brain\Common\Form\Type\Normalisation\PhoneNumberType
+     * @covers \Brain\Common\Form\Type\Normalisation\PostalCodeType
      */
     public function withNullValueResolveNull(): void
     {
         $builder = $this->factory->createBuilder();
-        $builder->add('number', PhoneNumberType::class);
+        $builder->add('postcode', PostalCodeType::class);
 
         $form = $builder->getForm();
 
         $data = [
-            'number' => null,
+            'postcode' => null,
         ];
 
         $form->submit($data);
 
         $expected = [
-            'number' => null,
+            'postcode' => null,
         ];
 
         self::assertEquals($expected, $form->getData());
@@ -49,25 +49,25 @@ final class PhoneNumberTypeTest extends TypeTestCase
      * @group form
      * @group normalisation
      *
-     * @covers \Brain\Common\Form\Type\Normalisation\PhoneNumberType
+     * @covers \Brain\Common\Form\Type\Normalisation\PostalCodeType
      */
     public function withValueAndCountryIsoNullReturnNull(): void
     {
         $builder = $this->factory->createBuilder();
-        $builder->add('number', PhoneNumberType::class, [
+        $builder->add('postcode', PostalCodeType::class, [
             'country_iso' => null,
         ]);
 
         $form = $builder->getForm();
 
         $data = [
-            'number' => '+(932) 234222d3 2324',
+            'postcode' => 'INVALID',
         ];
 
         $form->submit($data);
 
         $expected = [
-            'number' => null,
+            'postcode' => null,
         ];
 
         self::assertEquals($expected, $form->getData());
@@ -80,25 +80,25 @@ final class PhoneNumberTypeTest extends TypeTestCase
      * @group form
      * @group normalisation
      *
-     * @covers \Brain\Common\Form\Type\Normalisation\PhoneNumberType
+     * @covers \Brain\Common\Form\Type\Normalisation\PostalCodeType
      */
-    public function withInvalidPhoneNumberResolveNull(): void
+    public function withInvalidPostalCodeResolveNull(): void
     {
         $builder = $this->factory->createBuilder();
-        $builder->add('number', PhoneNumberType::class, [
+        $builder->add('postcode', PostalCodeType::class, [
             'country_iso' => 'GB',
         ]);
 
         $form = $builder->getForm();
 
         $data = [
-            'number' => '+(932) 234222d3 2324',
+            'postcode' => 'INVALID',
         ];
 
         $form->submit($data);
 
         $expected = [
-            'number' => null,
+            'postcode' => null,
         ];
 
         self::assertEquals($expected, $form->getData());
@@ -111,25 +111,25 @@ final class PhoneNumberTypeTest extends TypeTestCase
      * @group form
      * @group normalisation
      *
-     * @covers \Brain\Common\Form\Type\Normalisation\PhoneNumberType
+     * @covers \Brain\Common\Form\Type\Normalisation\PostalCodeType
      */
-    public function withValidPhoneNumberStandardise(): void
+    public function withValidPostalCodeStandardise(): void
     {
         $builder = $this->factory->createBuilder();
-        $builder->add('number', PhoneNumberType::class, [
+        $builder->add('postcode', PostalCodeType::class, [
             'country_iso' => 'GB',
         ]);
 
         $form = $builder->getForm();
 
         $data = [
-            'number' => '07706260852',
+            'postcode' => 'W1W 5nq',
         ];
 
         $form->submit($data);
 
         $expected = [
-            'number' => '+447706260852',
+            'postcode' => 'W1W5NQ',
         ];
 
         self::assertEquals($expected, $form->getData());
@@ -142,12 +142,12 @@ final class PhoneNumberTypeTest extends TypeTestCase
      * @group form
      * @group normalisation
      *
-     * @covers \Brain\Common\Form\Type\Normalisation\PhoneNumberType
+     * @covers \Brain\Common\Form\Type\Normalisation\PostalCodeType
      */
-    public function withValidPhoneNumberPriorityResolver(): void
+    public function withValidPostalCodePriorityResolver(): void
     {
         $builder = $this->factory->createBuilder();
-        $builder->add('number', PhoneNumberType::class, [
+        $builder->add('postcode', PostalCodeType::class, [
             'country_iso' => 'NL',
             'country_iso_resolver' => function () {
                 return 'GB';
@@ -157,13 +157,13 @@ final class PhoneNumberTypeTest extends TypeTestCase
         $form = $builder->getForm();
 
         $data = [
-            'number' => '07706260852',
+            'postcode' => 'W1W 5nq',
         ];
 
         $form->submit($data);
 
         $expected = [
-            'number' => '+447706260852',
+            'postcode' => 'W1W5NQ',
         ];
 
         self::assertEquals($expected, $form->getData());
@@ -176,13 +176,13 @@ final class PhoneNumberTypeTest extends TypeTestCase
      * @group form
      * @group normalisation
      *
-     * @covers \Brain\Common\Form\Type\Normalisation\PhoneNumberType
+     * @covers \Brain\Common\Form\Type\Normalisation\PostalCodeType
      */
     public function canCountryIsoResolveFromOtherField(): void
     {
         $builder = $this->factory->createBuilder();
         $builder->add('iso', TextType::class);
-        $builder->add('number', PhoneNumberType::class, [
+        $builder->add('postcode', PostalCodeType::class, [
             'country_iso_resolver' => function (FormInterface $form) {
                 return $form->get('iso')->getData();
             },
@@ -192,14 +192,14 @@ final class PhoneNumberTypeTest extends TypeTestCase
 
         $data = [
             'iso' => 'NL',
-            'number' => '020 555 1111',
+            'postcode' => '2993 VD',
         ];
 
         $form->submit($data);
 
         $expected = [
             'iso' => 'NL',
-            'number' => '+31205551111',
+            'postcode' => '2993VD',
         ];
 
         self::assertEquals($expected, $form->getData());
