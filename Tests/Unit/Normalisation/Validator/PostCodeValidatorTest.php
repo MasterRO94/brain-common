904b35b4d629755c5837f4b569e94f346f541e30
Alex Moon <alex.moon@printed.com>
2018-06-26 16:49:44 +0100

    BRN-724 - tests! :) still a few more to do but this is a good start

--- Tests/Unit/Normalisation/Validator/PostCodeValidatorTest.php
+++ Tests/Unit/Normalisation/Validator/PostCodeValidatorTest.php
@@ -41,7 +41,7 @@ final class PostCodeValidatorTest extends TestCase
      * @group unit
      * @group normalisation
      *
-     * @covers \Brain\Common\Normalisation\PhoneNumber
+     * @covers \Brain\Common\Normalisa tion\PhoneNumber
      *
      * @param string $value
      * @param string $standardised
