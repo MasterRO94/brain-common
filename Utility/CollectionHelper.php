282f075ff5ed557ab84d81d93da038ca46446dde
Matthew Rose <matthew.rose@printed.com>
2018-07-27 16:09:19 +0100

    BRN-763 ~ Making fixtures work properly.

---
+++ Utility/CollectionHelper.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Brain\Common\Utility;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+use LogicException;
+use Traversable;
+
+/**
+ * A collection helper.
+ */
+final class CollectionHelper
+{
+    /**
+     * Convert any collection-like in to an array collection.
+     *
+     * @param Collection|mixed[]|Traversable $iterable
+     *
+     * @throws LogicException When the $iterable cannot be handled.
+     */
+    public static function covert($iterable): Collection
+    {
+        if ($iterable instanceof Collection) {
+            return $iterable;
+        }
+
+        if ($iterable instanceof Traversable) {
+            return new ArrayCollection(iterator_to_array($iterable));
+        }
+
+        if (is_array($iterable)) {
+            return new ArrayCollection($iterable);
+        }
+
+        throw new LogicException(implode(' ', [
+            'The iterable instance cannot be converted to a Collection.',
+            'Please check the variable given and make sure it is valid.',
+        ]));
+    }
+}
