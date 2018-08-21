20d761df2e653f88aa1fc4c79070871c72c5c767
Matthew Rose <matthew.rose@printed.com>
2018-08-21 11:06:36 +0100

    Ran some inspections against the code.

--- Workflow/WorkflowBuilderInterface.php
+++ Workflow/WorkflowBuilderInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Brain\Common\Workflow;
 
+use Symfony\Component\Workflow\Transition;
 use Symfony\Component\Workflow\Workflow;
 
 interface WorkflowBuilderInterface
