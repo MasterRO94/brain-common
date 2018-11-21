ab72a0bfc37840b69d7a22f7c42a4984fb6f37af
Matthew Rose <matthew.rose@printed.com>
2018-11-21 14:38:55 +0000

    Check the client is available for outsource.

---
+++ Database/Pagination/Adapter/PassThroughPaginatorAdapter.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brain\Common\Database\Pagination\Adapter;
+
+use ArrayIterator;
+use Pagerfanta\Adapter\AdapterInterface;
+use Traversable;
+
+/**
+ * {@inheritdoc}
+ */
+final class PassThroughPaginatorAdapter implements AdapterInterface
+{
+    private $data;
+    private $total;
+
+    /**
+     * @param mixed[] $data
+     */
+    public function __construct(array $data, int $total)
+    {
+        $this->data = $data;
+        $this->total = $total;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNbResults(): int
+    {
+        return $this->total;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSlice($offset, $length): Traversable
+    {
+        return new ArrayIterator($this->data);
+    }
+}
