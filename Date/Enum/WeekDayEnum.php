f1226018640b77f1a2749848cb0e6d882aeaa3a7
Matthew Rose <matthew.rose@printed.com>
2019-06-10 15:55:22 +0100

    Removing opening time interface and refactoring.

--- Date/Enum/WeekDayEnum.php
+++ Date/Enum/WeekDayEnum.php
@@ -1,43 +1,19 @@
 <?php
 
-namespace Brain\Common\Date\Enum;
+declare(strict_types=1);
 
-use Brain\Common\Enum\AbstractEnum;
+namespace Brain\Common\Date\Enum;
 
 /**
  * Days of the week.
  */
-final class WeekDayEnum extends AbstractEnum
+final class WeekDayEnum
 {
-    const DAY_SUNDAY = 0;
-    const DAY_MONDAY = 1;
-    const DAY_TUESDAY = 2;
-    const DAY_WEDNESDAY = 3;
-    const DAY_THURSDAY = 4;
-    const DAY_FRIDAY = 5;
-    const DAY_SATURDAY = 6;
-
-    /**
-     * {@inheritdoc}
-     */
-    protected static function getTranslationPrefix(): string
-    {
-        return 'day';
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected static function getValues(): array
-    {
-        return [
-            self::DAY_SUNDAY,
-            self::DAY_MONDAY,
-            self::DAY_TUESDAY,
-            self::DAY_WEDNESDAY,
-            self::DAY_THURSDAY,
-            self::DAY_FRIDAY,
-            self::DAY_SATURDAY,
-        ];
-    }
+    public const DAY_SUNDAY = 0;
+    public const DAY_MONDAY = 1;
+    public const DAY_TUESDAY = 2;
+    public const DAY_WEDNESDAY = 3;
+    public const DAY_THURSDAY = 4;
+    public const DAY_FRIDAY = 5;
+    public const DAY_SATURDAY = 6;
 }
