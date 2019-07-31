d097f50a638926974e01aaa0bfe45e75641ed75e
Matthew Rose <matthew.rose@printed.com>
2019-07-31 10:12:44 +0100

    Started messing with new response structure. Hopefully a little more efficient that the current one where it does a thousand database queries.

---
+++ Exception/Developer/MethodNotImplementedRuntimeException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Brain\Common\Exception\Developer;
+
+final class MethodNotImplementedRuntimeException extends \RuntimeException
+{
+    /**
+     * @return MethodNotImplementedRuntimeException
+     */
+    public static function create(string $method, string $class): self
+    {
+        return new self($method, $class);
+    }
+
+    public function __construct(string $method, string $class)
+    {
+        $message = 'The method "%s" on class "%s" cannot be called because it is not implemented.';
+        $message = sprintf($message, $method, $class);
+
+        parent::__construct($message);
+    }
+}
