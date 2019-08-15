847105bd84ed5f7233b41e65cb6a3664e8e046ac
Matthew Rose <matthew.rose@printed.com>
2019-08-15 14:27:56 +0100

    Removed unused import from refactoring.

--- Form/Type/Date/DateRangeType.php
+++ Form/Type/Date/DateRangeType.php
@@ -8,7 +8,6 @@ use Brain\Common\Date\Range\DateTimeRange;
 use Brain\Common\Date\Range\OpenEndedDateTimeRange;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
